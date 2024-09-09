d00c66a03d8d3a700122855478fda1cc3c0b430b
Mahdi Ghanbarpour <113532242+MahdiGhanbarpour@users.noreply.github.com>
2024-09-09 15:57:34 +0430

    Completing the CRUD of tasks

--- core/todo/urls.py
+++ core/todo/urls.py
@@ -6,4 +6,7 @@ app_name = "todo"
 urlpatterns = [
     path('', views.TodoListView.as_view(), name="task-list"),
     path('task/create/', views.TaskCreateView.as_view(), name="task-create"),
+    path('task/delete/<int:pk>', views.TaskDeleteView.as_view(), name="task-delete"),
+    path('task/toggle/<int:pk>', views.TaskToggleView.as_view(), name="task-toggle"),
+    path('task/update/<int:pk>', views.TaskUpdateView.as_view(), name="task-update"),
 ]
--- core/todo/views.py
+++ core/todo/views.py
@@ -1,19 +1,56 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
 from .models import Task
 from django.urls import reverse_lazy
 
 # Create your views here.
 class TodoListView(ListView):
+    '''
+    A view for showing list of tasks
+    '''
     model = Task
     context_object_name = "tasks"
-    template_name = "index.html"
+    template_name = "todo/index.html"
     
 class TaskCreateView(CreateView):
-    model=Task
+    '''
+    A view for creating new tasks
+    '''
+    model = Task
     fields = ["title"]
     success_url = reverse_lazy("todo:task-list")
     
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+class TaskDeleteView(DeleteView):
+    '''
+    A view for deleting tasks
+    '''
+    model = Task
+    success_url = reverse_lazy("todo:task-list")
+    
+class TaskToggleView(UpdateView):
+    '''
+    A view for changing is_done property of selected task
+    '''
+    model = Task
+    fields = []
+    success_url = reverse_lazy("todo:task-list")
+    
+    def form_valid(self, form) :
+        form.instance.is_done = not form.instance.is_done
+        form.save()
+        return super().form_valid(form)
+    
+class TaskUpdateView(UpdateView):
+    '''
+    A view for changing title of selected task
+    '''
+    model = Task
+    fields = ["title"]
+    template_name = "todo/update_task.html"
+    success_url = reverse_lazy("todo:task-list")
