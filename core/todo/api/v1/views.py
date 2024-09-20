d1d3be7bb26982eaf931bf0e5c49c010ff9627e4
Mahdi Ghanbarpour <113532242+MahdiGhanbarpour@users.noreply.github.com>
2024-09-20 18:16:21 +0430

    Creating fake data and fixing bugs

--- core/todo/api/v1/views.py
+++ core/todo/api/v1/views.py
@@ -13,17 +13,25 @@ class TaskModelViewSet(viewsets.ModelViewSet):
 
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = TaskSerializer
-    queryset = Task.objects.filter()
 
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = ["is_done"]
     search_fields = ["title"]
     ordering_fields = ["created_date"]
 
+    def get_queryset(self):
+        return Task.objects.filter(author=self.request.user)
+
     @action(methods=["GET", "POST"], detail=True, url_path="change-status")
     def toggle_status(self, request, pk):
         """A extra action to toggle tasks status"""
         task = self.get_object()
+
+        if task.author != request.user:
+            return Response(
+                {"detail": "You are not allowed to change the status of this task."},
+                status=403,
+            )
 
         task.is_done = not task.is_done
         task.save()
